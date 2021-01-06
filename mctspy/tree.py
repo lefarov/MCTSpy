1cb90f81ef07ab92becea12906dfde9e14601dd2
Maksym Lefarov <mlefarov@gmail.com>
2021-01-06 12:46:27 +0100

    Backup is probably implemented.

--- mctspy/tree.py
+++ mctspy/tree.py
@@ -21,36 +21,54 @@ class ChanceNode:
 
 
 
-def ucb_action(decision_node):
-    """ Select the action in decision node according to UCB formula
+def ucb_action(decision_node: DecisionNode):
+    """ Select the action in decision node according to UCB formula.
     """
     pass
+
+
+def random_action(decision_node: DecisionNode):
+    """ Choose actoins randomly at every decision node.
+    """
+    return random.choice(tuple(decision_node.children.keys()))
 
 
 
 class MCST:
 
-    def __init__(self, num_iterations, state_value_estimator):
+    def __init__(
+        self, num_iterations: int, action_selector: Callable, state_value_estimator: Callable
+    ) -> None:
+        
         self.num_iterations = num_iterations
+        self.action_selector = action_selector
         self.state_value_estimator = state_value_estimator
 
         self.root = None
         self.stack = None
 
     def build_tree(self, state: Hashable):
-        # Reset tree
         self.root = DecisionNode(state, 0, {})
         self.stack = deque()
 
         for i in range(self.num_iterations):
-            value = self.expand(self.root, self.stack, self.state_value_estimator)
-            self.backup(value, self.stack)
+            # Assert that every iteration is started with clear stack
+            assert not self.stack
+
+            # Rollout siulation and expand tree
+            value = MCST.expand(self.root, self.stack, self.state_value_estimator)
+
+            # Backup the resulting value
+            MCST.backup(value, self.stack)
 
     @staticmethod
-    def expand(node: DecisionNode, stack: deque, state_value_estimator: Callable) -> float: 
+    def expand(
+        node: DecisionNode, stack: deque, action_selector: Callable, state_value_estimator: Callable
+    ) -> float: 
+        
         # Start at provided node (root)
         current_node = node
-        
+        # Go untill the terminal state
         while not current_node.state.is_end_of_game():
             # Get available actions
             available_actions = set(current_node.state.enumerate_moves())
@@ -60,6 +78,7 @@ class MCST:
                 # Sample random from untried actions
                 action = random.choice(tuple(available_actions - current_node.children.keys()))
                 # Advance simulation
+                # TODO: implement intermediate rewards for common RL interface
                 next_state = current_node.state.apply_move(action)
 
                 # Append nodes to the tree
@@ -71,17 +90,16 @@ class MCST:
                 stack.append(chance_node)
 
                 # Rollout simulation till the end using default policy
-                # TODO: should we implement off tree insertion?
+                # TODO: implement off tree insertion
                 return state_value_estimator(next_state)
         
-            # Select the best action according to UCB
-            action = ucb(current_node)
+            # Select the best action according to provided policy
+            action = action_selector(current_node)
             # Advance simulation
             next_state = current_node.state.apply_move(action)
             
             # Increment visites of Chance Node
             chance_node = current_node.children[action]
-            chance_node.visits += 1
             # Append nodes to the tree
             if next_state not in chance_node.children:
                 decision_node = DecisionNode(next_state, 0, {})
@@ -97,5 +115,10 @@ class MCST:
 
     @staticmethod
     def backup(value: float, stack: deque):
-        # Go in reverse over self.stack and update nodes
-        pass
+        # Go in reverse over stack and update nodes
+        while stack:
+            chance_node = deque.pop()
+
+            # Accumulate value and counts
+            chance_node.visits += 1
+            chance_node.value += value
