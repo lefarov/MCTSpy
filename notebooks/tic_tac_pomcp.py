6bd9a141499dafdfdf4ca1bd36f10457acd91039
Maksym Lefarov <mlefarov@gmail.com>
2021-09-25 17:25:42 +0200

    Some bug fixes.

--- notebooks/tic_tac_pomcp.py
+++ notebooks/tic_tac_pomcp.py
@@ -6,22 +6,23 @@ from mctspy.tree import POMCP, DecisionNode
 from mctspy.utilities import random_rollout_value
 from simulations.tic_tac import TicTac
 
-N = 5
+n_games = 5
 wins = 0
 verbose = False
 
 game = TicTac()
+n_iters = 10000
 seed = 1337
 my_agent_id = 0  # We play for X
 
 state_value_estimator = partial(random_rollout_value, env=game, seed=seed)
-mcts = POMCP(game, uct_action, state_value_estimator, 1000)
+mcts = POMCP(game, uct_action, state_value_estimator, n_iters)
 
 def report(message):
     if verbose:
         print(message)
 
-for i in range(N):
+for i in range(n_games):
 
     state, _ = game.get_initial_state()
     mcts_root = DecisionNode(None, 0, {}, state.nextAgentId, [state], [None])
@@ -49,6 +50,7 @@ for i in range(N):
         # Move into Decision Node for oponnent's "sense"
         node = node.children[action].children[observation]
 
+        # --- This part is hidden in the true simulator ---
         # Opponent's random "sense"
         action_sense = random.choice(tuple(game.enumerate_actions(state)))
         state, observation_sense, reward, agent_id = game.step(state, action_sense)
@@ -64,12 +66,14 @@ for i in range(N):
         # 1. In Blind Chess we don't get opponents "sense" action and observation
         # 2. Even if would get them, what if we didn't add the node in our tree
         #    which was actually selected by opponenet? What would happen to belief state?
+        history = node.history
         node = node.children[action_sense].children[observation_sense]
-        node = node.children[action_move].chilldren[observation_move]
+        node = node.children[action_move].children[observation_move]
+        node.history = history
 
 
     report(f"Winner: {state.winnerId}")
     if state.winnerId == 0:
         wins += 1
 
-print(f"Winrate: {wins / N}")
+print(f"Winrate: {wins / n_games}")
