07867e22a498c04741b9b9055c7511f4bc9860e4
Maksym Lefarov <mlefarov@gmail.com>
2021-10-12 12:09:08 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="mcts-py", # Replace with your own username
+    name="mctspy", # Replace with your own username
     version="0.0.1",
-    author="Maksym Lefaorv",
+    author="Maksym Lefarov",
     author_email="mlefarov@gmail.com",
-    description="MCTS implementaion in Python",
+    description="MCTS implementation in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lefarov/MCTSpy",
-    packages=find_packages(),    
+    packages=find_packages(exclude=["docs", "tests"]),    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
-    # install_requires=[
-    #     "numpy >= 1.11.1", 
-    #     "matplotlib >= 1.5.1".
-    # ],
+    python_requires='>=3.7',
 )
