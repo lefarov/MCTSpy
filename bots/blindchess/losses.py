663848ee5aaf8215864f92930d8c42ed96a94e51
Maksym Lefarov <mlefarov@gmail.com>
2021-10-18 14:28:07 +0200

    Combined loss is refactored.

--- bots/blindchess/losses.py
+++ bots/blindchess/losses.py
@@ -15,7 +15,7 @@ def q_loss(
     q_values_selected = q_values.gather(-1, act)
 
     # Select optimal values for the next time step
-    q_opt_next = torch.max(q_values_next, dim=-1, keepdim=True)
+    q_opt_next, _ = torch.max(q_values_next, dim=-1, keepdim=True)
     if q_values_next_estimate is not None:
         # Double Q idea: select the optimum action for the observation at t+1
         # using the trainable model, but compute it's Q value with target one
@@ -38,6 +38,10 @@ class CombinedLoss(torch.nn.Module):
         model: torch.nn.Module,
         model_target: torch.nn.Module,
         opponent_loss: torch.nn.Module,
+        memory_length: int=50,
+        obs_shape: tuple=(8, 8, 13),
+        sense_num: int=64,
+        move_num: int=64*64,
         discount: float=1.0,
         double_q: bool=True,
         mask_q: bool=True,
@@ -53,81 +57,90 @@ class CombinedLoss(torch.nn.Module):
         self.mask_q = mask_q
         self.weights = weights
 
-    def forward_move(
+        # Indices for sense and move actions in the batch of data
+        self.sense_ind = 0
+        self.move_ind = 1
+
+        # Dimensions for observation. We'll need them to cast batch to the right shape
+        self.obs_shape = (memory_length, *obs_shape)
+        self.sense_num = sense_num
+        self.move_num = move_num
+
+    def forward(
         self,
         obs: torch.Tensor,
         act: torch.Tensor,
         rew: torch.Tensor,
         done: torch.Tensor,
         obs_next: torch.Tensor,
-        mask_next: torch.Tensor,
+        act_next_mask: torch.Tensor,
         act_opponent: torch.Tensor=None,
         info_dict: dict=None,
     ):
-        # For the move data we train opponent action prediction head and Q head for move actions
-        *_, q_values, pred_act_opponent = self.model(obs)
-        opponent_loss = self.opponent_loss(pred_act_opponent, act_opponent.squeeze(-1))
+        # Merge action type and batch dimension since torch.Conv3d
+        # doesn't support "arbitrary-batched" semantics (WTF?).
+        obs = obs.flatten(0, 1)
+        obs_next = obs_next.flatten(0, 1)
+
+        # Feedforward all data in batch through the trainable model.
+        _, sense_q, move_q, pred_act_opponent = self.model(obs)
+        sense_q = sense_q.view(2, -1, self.sense_num)
+        move_q = move_q.view(2, -1, self.move_num)
+        pred_act_opponent = pred_act_opponent.view(2, -1, self.move_num)
+
+        # Compute opponent's move prediction loss.
+        opponent_loss = self.opponent_loss(
+            pred_act_opponent[self.move_ind], act_opponent[self.move_ind].squeeze(-1),
+        )
 
         info_dict["opponent_loss"] = opponent_loss
         loss = self.weights[0] * opponent_loss
 
         with torch.no_grad():
-            q_values_next = self.model_target.move(obs_next)
-            # TODO: double check if we need grad-flow through the estimate for Double-Q
-            q_values_next_estimate = None
+            # Predict sense and move Q-values for the next time-step
+            _, sense_q_next, move_q_next, _ = self.model_target(obs_next)
+            sense_q_next = sense_q_next.view(2, -1, self.sense_num)
+            move_q_next = move_q_next.view(2, -1, self.move_num)
+
+            sense_q_next_estimate, move_q_next_estimate = None, None
             if self.double_q:
                 # Compute Q estimate for the next time-step using trainable model (used in Double-Q)
-                q_values_next_estimate = self.model.move(obs_next)
+                _, sense_q_next_estimate, move_q_next_estimate, _ = self.model(obs_next)
+                sense_q_next_estimate = sense_q_next_estimate.view(2, -1, self.sense_num)
+                move_q_next_estimate = move_q_next_estimate.view(2, -1, self.move_num)
 
-            # Mask Q-values if masking is available
             if self.mask_q:
                 # Surprisingly that works for batches :)
-                q_values_next[mask_next == 0.0] = torch.finfo(q_values.dtype).min
+                move_q_next[act_next_mask == 0.0] = torch.finfo(move_q_next.dtype).min
                 if self.double_q:
-                    q_values_next_estimate[mask_next == 0.0] = torch.finfo(q_values.dtype).min
+                    move_q_next_estimate[act_next_mask == 0.0] = torch.finfo(move_q_next_estimate.dtype).min
 
-        move_loss = q_loss(
-            q_values,
-            q_values_next,
-            act,
-            rew,
-            done,
-            q_values_next_estimate,
+        # Compute Q-loss for sense action
+        sense_loss = q_loss(
+            sense_q[self.sense_ind],
+            sense_q_next[self.sense_ind],
+            act[self.sense_ind].unsqueeze(-1),
+            rew[self.sense_ind].unsqueeze(-1),
+            done[self.sense_ind].unsqueeze(-1),
+            sense_q_next_estimate[self.sense_ind] if self.double_q else None,
             self.discount,
         )
 
-        info_dict["move_loss"] = opponent_loss
-        loss += self.weights[1] * move_loss
+        info_dict["sense_loss"] = sense_loss
+        loss += self.weights[1] * sense_loss
+
+        # Compute Q-loss for move action
+        move_loss = q_loss(
+            move_q[self.move_ind],
+            move_q_next[self.move_ind],
+            act[self.move_ind].unsqueeze(-1),
+            rew[self.move_ind].unsqueeze(-1),
+            done[self.move_ind].unsqueeze(-1),
+            move_q_next_estimate[self.move_ind] if self.double_q else None,
+            self.discount,
+        )
+
+        info_dict["move_loss"] = move_loss
+        loss += self.weights[2] * move_loss
 
         return loss
-
-    def forward_sense(
-        self,
-        obs: torch.Tensor,
-        act: torch.Tensor,
-        rew: torch.Tensor,
-        done: torch.Tensor,
-        obs_next: torch.Tensor,
-        info_dict: dict=None
-    ):
-        # For sense actions we train only Q head for sense actions
-        q_values = self.model.sense(obs)
-
-        with torch.no_grad():
-            q_values_next = self.model_target.sense(obs_next)
-            q_values_next_estimate = None
-            if self.double_q:
-                q_values_next_estimate = self.model.sense(obs_next)
-
-        loss = q_loss(
-            q_values,
-            q_values_next,
-            act,
-            rew,
-            done,
-            q_values_next_estimate,
-            self.discount,
-        )
-
-        info_dict["sense_loss"] = loss
-        return loss * self.weights[-1]
